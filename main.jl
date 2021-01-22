1561a7162f3b3f3e08a09803cae9cf825e7afc2e
HiroshiTA <74251623+HiroshiTA@users.noreply.github.com>
2021-01-22 13:30:58 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -6,47 +6,57 @@ using LinearAlgebra
 #B = [57, 20, -4]
 #C = [0, 0, 0]
 
-function Método_Jacobi(A, B, C, max_iter = 100, E = 1e-3) #C é o vetor do chute inicial ou de zeros.
-    m,n = size(A)
-    k = 1    #número de iterações 
-    v = zeros(0)
-    erroR = E
-    while (k <= max_iter) || (erroR < E)
+using LinearAlgebra
+
+function Método_Jacobi(A, B, C, max_iter = 100, ϵ= 1e-3) #C é o vetor do chute inicial ou de zeros.
+    m,n = size(A)  
+    i = 1
+    j = 1
+    k = 1     #número de iterações
+    v = zeros(0)    #vetor que recebe os x1, x2, ..., xn.
+    erro = 1
+    print(ϵ)
+    while (k <= max_iter) || (erro > ϵ)
         i = 1
         j = 1
-        v = zeros(0)   #vetor que recebe os x1, x2, ..., xn.
+        v = zeros(0)
         while i <= m
-            a = A[i,j]        #elementos da Diagonal Principal
+            a = A[i,j]                #elementos da Diagonal Principal
             b = B[j]
-            o = (b/a)     
+            o = (b/a) 
             E = A[i,:]
             E[i] = 0
-            p = (dot(E,C))/a     
-            x = o - p
-            push!(v,x)
+            n = (dot(E,C))/a     
+            x = o - n
+            push!(v, x)
             i = i + 1
             j = j + 1
+            println(a)
         end
+        println(v)
+              
+        maiorx = abs(v[1])
+        println("maior x = $maiorx")
         
-        # Acharemos o maior elemento do Vetor de v e a maior distância entre o chute inicial de x e os valores achados.
-        maiorx = abs(v[1])               
+        distancia = abs.(v - C)
+        println("|x^(k) - x^(k-1)| = $distancia")
         distancia = abs.(v - C)
         maiord = distancia[1] 
         
-        #Para o maior x
         for h = 2:length(v)
             if maiorx < abs(v[h])
                 maiorx = abs(v[h])
             end
         end
         
-        # Para a maior distância
         for l = 2:length(distancia)
             if maiord < abs(distancia[l])
                 maiord = abs(distancia[l])
             end
         end
-        erroR = maiord / maiorx             #Erro Relativo
+        
+        erro = maiord / maiord
+
         C = v
         k = k + 1
     end
