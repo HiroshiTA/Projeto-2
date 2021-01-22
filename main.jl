940260748c0cded637df8a785bc961cd6d86edce
HiroshiTA <74251623+HiroshiTA@users.noreply.github.com>
2021-01-22 15:20:06 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -62,15 +62,16 @@ function Método_Jacobi(A, B, C, max_iter = 100, ϵ= 1e-3) #C é o vetor do chut
 end
 
 
-function Método_Gauss_Seidel(A, B, C, max_iter = 100,  = 1e-3) #C é o vetor do chute inicial ou de zeros.
+function Método_Gauss_Seidel(A, B, C, max_iter = 100, ϵ = 1e-3) #C é o vetor do chute inicial ou de zeros.
     m,n = size(A)
     k = 1    #número de iterações 
     v = zeros(0)
-    erroR = E
-    while (k <= max_iter) || (erroR < E)
+    erro = 1.0
+    while (k <= max_iter) && (erro > ϵ)
         i = 1
         j = 1
         v = zeros(0)   #vetor que recebe os x1, x2, ..., xn.
+        C2 = copy(C)
         while i <= m
             a = A[i,j]        #elementos da Diagonal Principal
             b = B[j]
@@ -83,29 +84,37 @@ function Método_Gauss_Seidel(A, B, C, max_iter = 100,  = 1e-3) #C é o vetor do
             C[i] = x
             i = i + 1
             j = j + 1
+            println(a)
         end
+        println(v)
         
-        # Acharemos o maior elemento do Vetor de v e a maior distância entre o chute inicial de x e os valores achados.
-        maiorx = abs(v[1])               
-        distancia = abs.(v - C)
-        maiord = distancia[1] 
-        
-        #Para o maior x
+        #Para achar o maior dos x dentro do vetor v
+        maiorx = abs(v[1])
         for h = 2:length(v)
             if maiorx < abs(v[h])
                 maiorx = abs(v[h])
             end
         end
+        println("maior x = $maiorx")
+        println(C2)
         
-        # Para a maior distância
+        #A maior distancia entre as soluções dos x".
+        
+        distancia = abs.(v - C2)
+        println("|x^(k) - x^(k-1)| = $distancia")
+        maiord = distancia[1]         
         for l = 2:length(distancia)
             if maiord < abs(distancia[l])
                 maiord = abs(distancia[l])
             end
         end
-        erroR = maiord / maiorx             #Erro Relativo
-        C = v
+        
+        erro = maiord / maiorx          #Erro Relativo
+        println("Erro Relativo = $erro")
+        C = v                           #Atualização da nova matriz: x^k-1 = x^k.
+        println("x^k = $C")
         k = k + 1
+        println("")
     end
     return v
 end
