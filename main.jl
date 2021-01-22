fd1f59a0bac05eb51684e7b13beeccc503921d5a
HiroshiTA <74251623+HiroshiTA@users.noreply.github.com>
2021-01-22 16:40:37 -0300

    Update main.jl

--- main.jl
+++ main.jl
@@ -119,4 +119,54 @@ function Método_Gauss_Seidel(A, B, C, max_iter = 100, ϵ = 1e-3) #C é o vetor 
     return v
 end
 
-
+function Método_SOR(A, B, C, max_iter = 100, ω = 1.25, ϵ = 1e-4) #C é o vetor do chute inicial ou de zeros.
+    m,n = size(A)
+    k = 1    #número de iterações 
+    v = zeros(0)
+    erro = 1.0
+    while (k <= max_iter) && (erro > ϵ)
+        i = 1
+        j = 1
+        v = zeros(0)   #vetor que recebe os x1, x2, ..., xn.
+        C2 = copy(C)
+        while i <= m
+            a = A[i,j]        #elementos da Diagonal Principal
+            b = B[j]     
+            E = A[i,:]
+            R = b - dot(E,C) 
+            x = C[i] + ω * R / a
+            push!(v,x)
+            C[i] = x
+            i = i + 1
+            j = j + 1
+            println(R)
+        end
+        
+        #Para achar o maior dos x dentro do vetor v
+        maiorx = abs(v[1])
+        for h = 2:length(v)
+            if maiorx < abs(v[h])
+                maiorx = abs(v[h])
+            end
+        end
+        println("maior x = $maiorx")
+        
+        #A maior distancia entre as soluções dos x".
+        
+        distancia = abs.(v - C2)      
+        maiord = distancia[1]         
+        for l = 2:length(distancia)
+            if maiord < abs(distancia[l])
+                maiord = abs(distancia[l])
+            end
+        end
+        
+        erro = maiord / maiorx          #Erro Relativo
+        println("Erro Relativo = $erro")
+        C = v                           #Atualização da nova matriz: x^k-1 = x^k.
+        println("x^k = $C")
+        k = k + 1
+        println("")
+    end
+    return v
+end
